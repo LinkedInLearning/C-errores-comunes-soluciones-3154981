a503bd7340d883e00204f4b15e40aa05ea7196f7
Eliezer López <contacto@eliezerlopez.com>
2022-03-24 20:56:37 +0100

    Ejercicio Capítulo 9
    Ejercicio del capítulo nueve en su versión inicial (con fallos) y final (con el error ya corregido).

---
+++ Capitulo 9/olvidar_instruccion_system_pause/olvidar_instruccion_system_pause_f.c
@@ -0,0 +1,16 @@
+/*
+ * Curso "C: Errores comunes y como solucionarlos"
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    puts("¡Hola! Soy @EliezerLopez. Gracias por realizar mi curso en LinkedIn Learning :)");
+    
+    system("pause");
+    return 0;
+}
---
+++ Capitulo 9/olvidar_instruccion_system_pause/olvidar_instruccion_system_pause_i.c
@@ -0,0 +1,14 @@
+/*
+ * Curso "C: Errores comunes y como solucionarlos"
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <stdio.h>
+
+int main(){
+
+    puts("¡Hola! Soy @EliezerLopez. Gracias por realizar mi curso en LinkedIn Learning :)");
+    
+    return 0;
+}
