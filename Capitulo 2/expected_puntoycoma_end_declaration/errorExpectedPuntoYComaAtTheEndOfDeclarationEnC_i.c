be47f45a841be29ab02b772bf4238b2031058212
Eliezer López <contacto@eliezerlopez.com>
2022-03-24 20:16:05 +0100

    Ejercicios Capítulo 2
    Ejercicios del capítulo dos en sus versiones inicial (con fallos) y final (con los errores ya corregidos).

---
+++ Capitulo 2/expected_puntoycoma_end_declaration/errorExpectedPuntoYComaAtTheEndOfDeclarationEnC_i.c
@@ -0,0 +1,47 @@
+/*
+ * Curso "C: Errores comunes y como solucionarlos"
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+ 
+ #include <stdio.h>
+
+float suma(float a, float b);
+float resta(float a, float b);
+float producto(float a, float b);
+float division(float a, float b);
+
+int main(){
+
+    float a,b
+
+    printf("Introduce el operando 1: ");
+    scanf("%f", &a);
+
+    printf("Introduce el operando 2: ");
+    scanf("%f", &b)
+
+    puts("Los resultados de las operaciones son: ");
+    printf("- Suma: %.2f\n", suma(a,b))
+    printf("- Resta: %.2f\n", resta(a,b));
+    printf("- Producto: %.2f\n", producto(a,b))
+    printf("- División: %.2f\n", division(a,b));
+
+    return 0
+}
+
+float suma(float a, float b){
+    return a+b;
+}
+
+float resta(float a, float b){
+    return a-b
+}
+
+float producto(float a, float b){
+    return a*b;
+}
+
+float division(float a, float b){
+    return a/b;
+}
---
+++ Capitulo 2/inclusion_erronea_simbolos_puntoycoma/inclusion_erronea_simbolos_puntoycoma_i.c
@@ -0,0 +1,33 @@
+/*
+ * Curso "C: Errores comunes y como solucionarlos"
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <stdio.h>
+
+int main(){
+
+    float resistencia1, resistencia2; resistencia3, resistenciaGlobal;
+    char tipoDeConexion;
+
+    puts("¡Hola! Este programa calcula el valor global de la resistencia formada por 3 resistencias proporcionadas por el usuario.");
+
+    printf("Introduce el valor de la resistencia 1: ");
+    scanf("%f", &resistencia1);
+    printf("Introduce el valor de la resistencia 2: ");
+    scanf("%f", &resistencia2);
+    printf("Introduce el valor de la resistencia 3: ");
+    scanf("%f", &resistencia3);
+
+    printf("¿Las resistencias están conectadas en serie (s) o en paralelo (p)?: ");
+    getchar();
+    scanf("%c"; &tipoDeConexion);
+
+    if(tipoDeConexion == 's') resistenciaGlobal = resistencia1 + resistencia2 + resistencia3;
+    else resistenciaGlobal = 1;/((1/resistencia1)+(1/resistencia2)+(1/resistencia3));
+
+    printf("\nEl valor de la resistencia global es de %.2f Ohmios."; resistenciaGlobal);
+
+    return 0;
+}
